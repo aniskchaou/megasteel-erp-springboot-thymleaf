1aac287641ed8ea294a1b7f65c515bf8a2f585e1
Anis KCHAOU <kchaouanis26@gmail.com>
2021-09-28 23:01:21 +0200

    downgrade java version to 8

--- src/test/java/com/dev/delta/SpringbootMegasteelThymleafCodecanyonApplicationTests.java
+++ src/test/java/com/dev/delta/SpringbootMegasteelThymleafCodecanyonApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootMegasteelThymleafCodecanyonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
